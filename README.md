b8c3eefa2afe0570ba5238ed11ad73bba3b34d25
Carmelo C <carmelo.califano@gmail.com>
2024-06-03 15:32:38 +0200

    First real commit, the whole app should be usable now

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # FastLLMAPI
 API-first frontend to Ollama (backend)
+
+### Install
+```
+$ git clone git@github.com:carmelo0x63/FastLLMAPI.git
+
+$ python3 -m venv .
+
+$ source bin/activate
+
+$ python3 -m pip install --upgrade pip setuptools wheel
+
+$ python3 -m pip install --upgrade fastapi Flask requests
+```
+
+### Run
+In two separate terminal windows, run the following commands:
+1. `python3 main_app.py`
+2. `python3 gui_app.py`
+The interface will be available on the host where the applications are run, on port 5000:
+```
+http://<ip_address>:5000/
+```
---
+++ main_app.py
@@ -0,0 +1,51 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+import json
+import requests
+
+app = FastAPI(debug=True)
+
+class Itemexample(BaseModel):
+    name: str
+    prompt: str
+    instruction: str
+    is_offer: Union[bool, None] = None
+
+class Item(BaseModel):
+    model: str
+    prompt: str
+
+urls = {
+    "llama3": "http://localhost:11434/api/generate"
+}
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/chat/{llms_name}")
+def update_item(llms_name: str, item: Item):
+    if llms_name in urls:
+        url = urls[llms_name]
+        payload = {
+            "model": item.model,
+            "prompt": item.prompt,
+            "stream": False
+        }
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+        if response.status_code == 200:
+            return {"data": response.json(), "llms_name": llms_name}  # Use response.json() if the response is JSON
+        else:
+            print("error:", response.status_code, response.text)
+            return {"item_name": item.model, "error": response.status_code, "data": response.text}
+    return {"item_name": item.model, "llms_name": llms_name, "error": "Unsupported model"}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
